497cc6e539322114d952ac9f7dd8bfcc4bb44283
harcioger <harciog_hams@mail.ru>
2019-11-13 08:59:32 +0300

    вернул старый код

--- functions/require/products_items.php
+++ functions/require/products_items.php
@@ -370,11 +370,27 @@ if(isset($_GET['error']))
 
 		</article>
 		<script type="text/javascript">
-                $(document).ready(function() {
-                    $('#products_list').DataTable( {
-                        ajax: '/api/myData'
-                    });
-                });
+				document.addEventListener('DOMContentLoaded', () => {
+
+					const getSort = ({ target }) => {
+							const order = (target.dataset.order = -(target.dataset.order || -1));
+							const index = [...target.parentNode.cells].indexOf(target);
+							const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+							const comparator = (index, order) => (a, b) => order * collator.compare(
+									a.children[index].innerHTML,
+									b.children[index].innerHTML
+							);
+
+							for(const tBody of target.closest('table').tBodies)
+									tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+
+							for(const cell of target.parentNode.cells)
+									cell.classList.toggle('sorted', cell === target);
+					};
+
+					document.querySelectorAll('.table_sort thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+
+					});
 		</script>
 	</div>
 </section>
